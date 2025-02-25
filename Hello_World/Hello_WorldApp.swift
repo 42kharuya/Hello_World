c0043b318cc0cfceb685222ec39fc0186d97d896
42kharuya <haruya.0411.k@gmail.com>
2025-02-26 01:42:27 +0900

    Initial Commit

---
+++ Hello_World/Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldApp.swift
+//  Hello_World
+//
+//  Created by 川﨑遥也 on 2025/02/26.
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hello_WorldTests/Hello_WorldTests.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldTests.swift
+//  Hello_WorldTests
+//
+//  Created by 川﨑遥也 on 2025/02/26.
+//
+
+import Testing
+@testable import Hello_World
+
+struct Hello_WorldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
